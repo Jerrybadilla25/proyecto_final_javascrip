5c29567e77a8a645cea3c2b9b37576f2152e3eff
Jerry Badilla <jerrybadilla@gmail.com>
2019-08-02 19:35:12 -0600

    eventos funcion

--- js/app.js
+++ js/app.js
@@ -1,28 +1,61 @@
-var a;
-var b;
-var c;
+var calculadora = {
+
+    pantalla: document.getElementById("display"),
+    valorpantalla ="0",
+    valor1 ="0",
+    valor2 ="0",
+    ultimoValor ="0",
+    resultadoFinal ="0",
+
+    init: (function(){
+        this.eventosFunciones();
+    }),
 
 
-funtion calculadora(){
-    var on = document.getElementById("on");   
-    var uno = document.getElementById("1");
-    var dos = document.getElementById("2");
-    var tres = document.getElementById("3");
-    var cuatro = document.getElementById("4");
-    var cinco = document.getElementById("5");
-    var seis = document.getElementById("6");
-    var siete = document.getElementById("7");
-    var ocho = document.getElementById("8");
-    var nueve = document.getElementById("9");
-    var cero = document.getElementById("0");
-    var punto = document.getElementById("punto");
-    var igual = document.getElementById("igual");
-    var menos = document.getElementById("menos");
-    var por = document.getElementById("por");
-    var dividido = document.getElementById("dividido");
-    var display = document.getElementById("display");
+    eventosFunciones: (){
+        document.getElementById("0").addEventListener("click", function(){calculadora.ingresoNumero("0"),});
+        document.getElementById("1").addEventListener("click", function(){calculadora.ingresoNumero("1"),});
+        document.getElementById("2").addEventListener("click", function(){calculadora.ingresoNumero("2"),});
+        document.getElementById("3").addEventListener("click", function(){calculadora.ingresoNumero("3"),});
+        document.getElementById("4").addEventListener("click", function(){calculadora.ingresoNumero("4"),});
+        document.getElementById("5").addEventListener("click", function(){calculadora.ingresoNumero("5"),});
+        document.getElementById("6").addEventListener("click", function(){calculadora.ingresoNumero("6"),});
+        document.getElementById("7").addEventListener("click", function(){calculadora.ingresoNumero("7"),});
+        document.getElementById("8").addEventListener("click", function(){calculadora.ingresoNumero("8"),});
+        document.getElementById("9").addEventListener("click", function(){calculadora.ingresoNumero("9"),});
+        document.getElementById("on").addEventListener("click", function(){calculadora.borrarPantalla(),});
+        document.getElementById("sign").addEventListener("click", function(){calculadora.cambiarSigno(),});
+        document.getElementById("punto").addEventListener("click", function(){calculadora.ingresarPunto(),});
+        document.getElementById("igual").addEventListener("click", function(){calculadora.verResultado(),});
+        document.getElementById("dividido").addEventListener("click", function(){calculadora.operaciones("/"),});
+        document.getElementById("por").addEventListener("click", function(){calculadora.operaciones("*"),});
+        document.getElementById("menos").addEventListener("click", function(){calculadora.operaciones("-"),});
+        document.getElementById("mas").addEventListener("click", function(){calculadora.operaciones("+"),});
+    },
+
+    ingresoNumero: function(valor){
+        if (this.valorpantalla.length < 8){
+            this.valorpantalla="0";
+            this.valorpantalla=this.valorpantalla+valor;
+        }else{
+            this.valorpantalla=this.valorpantalla+valor;
+        }
+
+        this.actualizarpantalla();
+    },
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
-uno.onclick = funtion(e){
-    display.textcontent = display.textcontent + "1";
-}
